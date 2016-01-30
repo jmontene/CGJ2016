aa9a6ba1e8f3176c011a135f1e06c31f3e7c6030
Zuil <zuilnorris@gmail.com>
2016-01-30 01:33:50 -0430

    meh

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,17 +7,30 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	public bool canMoveHorizontal;
+    public float fireRate = 1.0F;
+    private float nextFire = 0.0F;
 
+    public GameObject shot;
+
+    
 	// Use this for initialization
 	void Awake () {
 		rb = GetComponent<Rigidbody> ();
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    // Update is called once per frame
+    void FixedUpdate () {
 		float hMov = canMoveHorizontal ? Input.GetAxis ("Horizontal") : 0.0f;
 		float vMov = Input.GetAxis ("Vertical");
 
 		rb.velocity = new Vector3 (hMov * speed, 0.0f, vMov * speed);
-	}
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        {
+            Debug.Log("sdfsdfsd");
+            nextFire = Time.time + fireRate;
+            GameObject clone = Instantiate(shot, rb.position+new Vector3(-0.65f,0.0f,0.3f), rb.rotation) as GameObject;
+            clone.transform.parent = rb.transform;
+        }
+    }
 }
