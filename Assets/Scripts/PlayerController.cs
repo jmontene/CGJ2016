4d1a32bcb066dcf9c1ffa785539f8c7b916b49fe
Andres Mayo <aemc.64@gmail.com>
2016-01-29 22:28:06 -0430

    Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	public float speed;
+	public bool canMoveHorizontal;
+
+	// Use this for initialization
+	void Awake () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float hMov = canMoveHorizontal ? Input.GetAxis ("Horizontal") : 0.0f;
+		float vMov = Input.GetAxis ("Vertical");
+
+		rb.velocity = new Vector3 (hMov * speed, 0.0f, vMov * speed);
+	}
+}
