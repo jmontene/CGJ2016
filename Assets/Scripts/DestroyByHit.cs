a7349539a6ec3373e3730ff6f9d734c7deaaa6b4
Zuil <zuilnorris@gmail.com>
2016-01-30 03:47:50 -0430

    hello world

---
+++ Assets/Scripts/DestroyByHit.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByHit : MonoBehaviour
+{
+    private float score=0;
+    void OnTriggerEnter(Collider other)
+    {
+ 
+        switch (other.tag)
+        {
+            case "boundary":{
+                    return;
+                }
+            case "Enemy":{
+                    Destroy(other.gameObject);
+                    Destroy(gameObject);
+                    score += 250;
+                    break;
+                }
+            case "Player":
+                {
+                    Destroy(other.gameObject);
+                    Destroy(gameObject);
+                    break;
+                }
+            case "Ball":
+                {
+                    Destroy(other.gameObject);
+                    Destroy(gameObject);
+                    break;
+                }
+        }
+    }
+}
